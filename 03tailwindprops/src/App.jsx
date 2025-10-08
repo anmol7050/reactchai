d902716050e69cdf354cb77285c1431c39dba8d2
anmol7050 <anmolkumaryadav7050@gmail.com>
2025-10-09 00:49:29 +0530

    react basic

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-black paddin p-4 rounded-4xl">
+        Anmol Kumar Yadav
+      </h1>
+    </>
+  );
+}
+
+export default App;
